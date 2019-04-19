a6fe28a60b16d83dc50afdc3ddb2725eddf5d4ff
Pramodi Herath <pramodihansika@gmail.com>
2019-04-19 19:18:24 +0530

    commit4/19

--- client/src/app/BookingTulip/BookingTulip.component.ts
+++ client/src/app/BookingTulip/BookingTulip.component.ts
@@ -7,11 +7,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class BookingTulipComponent implements OnInit {
 
-  myForm = new FormGroup({
-    myDateYMD: new FormControl(new Date()),
-    myDateFull: new FormControl(new Date()),
-    myDateMDY: new FormControl(new Date())
-  });
+  
 
   constructor() { }
 
