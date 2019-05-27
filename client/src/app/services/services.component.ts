0d8186c45b8afe3ca3948f1180fb5b494acd7158
Pramodi Herath <pramodihansika@gmail.com>
2019-05-27 13:08:32 +0530

    commit5/27noon

--- client/src/app/services/services.component.ts
+++ client/src/app/services/services.component.ts
@@ -6,8 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent implements OnInit {
+  champagne='/assets/images/services.jpg';
+
+
+   drapingPrice:number=120000;
+   ChampagnePrice:number=2000;
+   dryIcePrice:number=3500;
+   milkRicePlatterPrice:number=2000;
+   decoPrice:number=25000;
+   screenPrice:number=6000;
+   tiffanyChairsPrice:number=200;
+   milkRicePortionPrice:number=250;
+
 
   constructor() { }
+ 
+
 
   ngOnInit() {
   }
